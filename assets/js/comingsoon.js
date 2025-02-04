1d2d6ce8c91576e24c65d4ead958f692f04c9c73
eyern <eyern254@gmail.com>
2025-02-04 17:41:48 +0300

    launch date change

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Feb 5, 2025 00:00:00").getTime();
+    const countDate = new Date("Feb 8, 2025 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
