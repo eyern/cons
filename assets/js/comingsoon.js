7f5647c7afee26a50921fe1c2e5b84b33b39a46d
eyern <eyern254@gmail.com>
2025-03-24 14:48:57 +0300

    launch

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("March 20, 2025 00:00:00").getTime();
+    const countDate = new Date("April 2, 2025 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
